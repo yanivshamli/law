b5defaf48dab8fd9844f5d3493553572f64d8b83
yanivshamli <yanivshamli@gmail.com>
2025-03-12 23:41:04 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('nav a');
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            const target = document.querySelector(link.getAttribute('href'));
+            if (target) {
+                target.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
